52bf88cc4456982ff3f4a2a0b1d6d7310731b28a
Marco <mgonz781@mtroyal.ca>
2023-03-20 09:56:54 -0600

    more changes to push

--- handlers/movieRouter.js
+++ handlers/movieRouter.js
@@ -11,18 +11,34 @@ const handleAllMovies = (app, Movie) => {
 };
 
 const handleLimitMovies = (app, Movie) => {
-
-};
-
-const handleSingleMovie = (app, Movie) => {
-    console.log("Hello");
-    app.get('/api/movies/:id', (req,resp) => {
-        Movie.find({ id: req.params.id })
+    app.get('/api/movies/limit/:num', (req,resp) => {
+        if (req.params.num >= 1 && req.params.num <= 200) {
+            Movie.find()
+            .limit(req.params.num)
             .then((data) => {
                 resp.json(data);
             })
             .catch((err) => {
-                resp.json({ message: "Unable to Connect to Books"});
+                resp.json({ message: "Unable to Connect to Movies"});
+            });
+        } else {
+            resp.json({ message: "Value is out of Range"});
+        }
+    });
+};
+
+const handleSingleMovie = (app, Movie) => {
+    app.get('/api/movies/:id', (req,resp) => {
+        Movie.find({ id: req.params.id })
+            .then((data) => {
+                if (data) {
+                    resp.json(data);
+                } else {
+                    resp.json({ messsage: "No Such Record Found"});
+                }
+            })
+            .catch((err) => {
+                resp.json({ message: "Unable to Connect to Movies"});
             });
     });
 };
@@ -31,29 +47,31 @@ const handleMovieTMDB = (app, Movie) => {
     app.get('/api/movies/tmdb/:tmdb_id', (req,resp) => {
         Movie.find({ tmdb_id: req.params.tmdb_id })
             .then((data) => {
-                resp.json(data);
+                if (data) {
+                    resp.json(data);
+                } else {
+                    resp.json({ message: "No Such Record Found"});
+                }
             })
             .catch((err) => {
-                resp.json({ message: "Unable to Connect to Books"});
+                resp.json({ message: "Unable to Connect to Movies"});
             });
     });
 };
 
 const handleMovieYear = (app, Movie) => {
-    console.log("Calling this from the link");
     app.get('/api/movies/year/:min/:max', (req,resp) => {
         Movie.find()
             .where("release_date")
             .gt(req.params.min)
             .lt(req.params.max)
-            .sort({title:1})
-            .select("title runtime")
+            .select("title")
             .exec()
             .then((data) => {
                 resp.json(data);
             })
             .catch((err) => {
-                resp.json({ message: "Unable to Connect to Books"});
+                resp.json({ message: "Unable to Connect to Movies"});
             })
     });
 };
