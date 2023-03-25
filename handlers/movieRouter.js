c71c8d133da9e07ae3f4b6035d4ff9fd0e03c5c3
marcogonzalez99 <MarcoGonzalezMG99@gmail.com>
2023-03-25 16:20:39 -0600

    more routing changes

--- handlers/movieRouter.js
+++ handlers/movieRouter.js
@@ -12,6 +12,12 @@ const handleAllMovies = (app, Movie) => {
 
 const handleLimitMovies = (app, Movie) => {
     app.get('/api/movies/limit/:num', (req,resp) => {
+
+        if (isNaN(req.params.num)) {
+            resp.json({ message: "Invalid input" });
+            return;
+        }
+
         if (req.params.num >= 1 && req.params.num <= 200) {
             Movie.find()
             .limit(req.params.num)
@@ -29,6 +35,12 @@ const handleLimitMovies = (app, Movie) => {
 
 const handleSingleMovie = (app, Movie) => {
     app.get('/api/movies/:id', (req,resp) => {
+
+        if (isNaN(req.params.id)) {
+            resp.json({ message: "Invalid input" });
+            return;
+        }
+
         Movie.find({ id: req.params.id })
             .then((data) => {
                 if (data.length > 0) {
@@ -45,6 +57,11 @@ const handleSingleMovie = (app, Movie) => {
 
 const handleMovieTMDB = (app, Movie) => {
     app.get('/api/movies/tmdb/:tmdb_id', (req,resp) => {
+
+        if (isNaN(req.params.tmdb_id)) {
+            resp.json({ message: "Invalid input" });
+            return;
+        }
         Movie.find({ tmdb_id: req.params.tmdb_id })
             .then((data) => {
                 if (data.length > 0) {
@@ -77,15 +94,88 @@ const handleMovieYear = (app, Movie) => {
 };
 
 const handleMovieAverage = (app, Movie) => {
+    app.get('/api/movies/ratings/:min/:max', (req, resp) => {
+        const minRating = req.params.min;
+        const maxRating = req.params.max;
 
+        if (isNaN(minRating) || isNaN(maxRating) || maxRating < minRating || maxRating > 10 || minRating < 0) {
+            resp.json({ message: "Invalid rating range" });
+            return;
+        }
+        Movie.find()
+        .where("ratings.average")
+        .gt(minRating)
+        .lt(maxRating)
+        .exec()
+        .then((data) => {
+            if (data.length > 0) {
+                resp.json(data);
+            } else {
+                resp.json({ message: "No Such Record Found"});
+            }
+        })
+        .catch((err) => {
+            resp.json({ message: "Unable to Connect to Movies"});
+        }); 
+    });
 };
 
 const handleMovieTitle = (app, Movie) => {
+    app.get('/api/movies/title/:text', (req, resp) => {
+        const textToFind = req.params.text;
+        // Case insensitive from https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+        const query = {
+            title: {
+                $regex: textToFind,
+                $options: 'i'
+            }
+        };
 
+        Movie.find(query)
+        .then((data) => {
+            if (data.length > 0) {
+                resp.json(data);
+            } else {
+                resp.json({ message: "No Movies found with the given title"});
+            }
+        })
+        .catch((err) => {
+            resp.json({ message: "Unable to connect to movies"})
+        });
+    });
 };
 
 const handleMovieGenre = (app, Movie) => {
+    app.get('/api/movies/genre/:name', (req, resp) => {
+        const genreToFind = req.params.name;
 
+        // Finding in an array from 
+        // https://www.mongodb.com/docs/manual/reference/operator/query/elemMatch/
+        const query = {
+            details: {
+                $elemMatch: {
+                    genres: {
+                        name: {
+                            $regex: genreToFind,
+                            $options: 'i'
+                        }
+                    }
+                }
+            }
+        };
+
+        Movie.find(query)
+        .then((data) => {
+            if (data.length > 0) {
+                resp.json(data);
+            } else {
+                resp.json({ message: "No Movies found with the provided genre"})
+            }
+        })
+        .catch((err) => {
+            resp.json({ message: "Unable to connect to movies"});
+        });
+    });
 };
 
 module.exports = {
