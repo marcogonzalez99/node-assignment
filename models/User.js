af92951beb58831b1f63156fda7ff9b19573d72b
Marco <mgonz781@mtroyal.ca>
2023-03-29 08:55:26 -0600

    created a user schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    id: Number,
+    details: {
+        firstname: String,
+        lastname: String,
+        city: String,
+        country: String
+    },
+    picture: {
+        large: String,
+        thumbnail: String
+    },
+    membership: {
+        date_joined: String,
+        last_update: String,
+        likes: Number
+    },
+    email: String,
+    password_bcrypt: String,
+    apikey: String,
+    favorites: List
+});
+
+module.exports = mongoose.model('User', userSchema, 'Users');
