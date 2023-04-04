6a030bfdf831aa9343175e66186248355fa61879
MatthewPham-hub <matthewpham26@gmail.com>
2023-04-04 02:21:13 -0600

    Added documentation to code

--- handlers/auth.js
+++ handlers/auth.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const UserModel = require('../models/User.js');
 
-// maps the passport fields to the names of fields in database
+// Maps passport fields to email and password fields in database.
 const localOpt = {
     usernameField : 'email',
     passwordField : 'password_bcrypt'
@@ -11,20 +11,19 @@ const localOpt = {
 // Strategy for validating login
 const strategy = new LocalStrategy(localOpt, async (email, password, done) => {
     try {
-        // Find the user in the DB associated with this email
+        // Finds the user matching the email. 
         const userChosen = await UserModel.findOne({ email: email });
 
         if ( !userChosen ) {
-            //If the user isn't found in the database, set flash message
+            //Set message to email not found if no user matches that email.
             return done(null, false, { message : 'Email not found'});
         }
-        // Validate password and make sure it matches the bcrypt digest
-        //stored in the database. If they match, return a value of true.
+        //Validate password for found user, if not found return error. 
         const validate = await userChosen.isValidPassword(password);
         if ( !validate ) {
             return done(null, false, { message : 'Wrong Password'});
         }
-        // Send the user information to the next middleware
+        //If logged in send successful message.
         return done(null, userChosen, { message : 'Logged in Successfully'});
     }
     catch (error) {
@@ -32,13 +31,10 @@ const strategy = new LocalStrategy(localOpt, async (email, password, done) => {
     }
 });
 
-// for localLogin, use our strategy to handle User login
+// Use strategy defined above for login. 
 passport.use('localLogin', strategy);
 
-// by default, passport uses sessions to maintain login status ...
-// you have to determine what to save in session via serializeUser
-// and deserializeUser. In our case, we will save the email in the
-// session data
+//Save email in session detail. 
 passport.serializeUser( (user, done) => done(null, user.email) );
 passport.deserializeUser(async (email, done) => {
     try {
